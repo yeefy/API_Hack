b20a853e6e90cd779bb9cda52873bebd34a0b732
yeefy <yftran@gmail.com>
2014-03-26 00:26:05 -0700

    upload 3

--- application.js
+++ application.js
@@ -3,11 +3,14 @@
 //var clientID = "a2755c30cab54e28a1d31896eee5333f";
 //var code = "dd8ef8eb6d104f99b1716fb145de508c";
 
-var accessToken = "285306793.a2755c3.6da79bffc3fa4a8b88eb3ee051058f4e";
-var instagramAPI = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + accessToken + "&callback=?&count=18";
 var tag = "";
 
+
 function getInstagram () {
+
+	var accessToken = "285306793.a2755c3.6da79bffc3fa4a8b88eb3ee051058f4e";
+	var instagramAPI = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + accessToken + "&callback=?&count=18";
+	
 	
 		$.ajax({
 			dataType:"jsonp",
@@ -18,8 +21,8 @@ function getInstagram () {
 						var photoImage = photo.data[i].images.low_resolution.url;
 						var photoCaption = photo.data[i].caption.text;
 						var photoLink = photo.data[i].link;
-						var photoContent = "<span><a href= " + photoLink + "  target='_blank'><img src= " + photoImage + " /><a/><h3 class= overlay>" + 					photoCaption + "<h3/><span/>"
-						$(".images").append(photoContent).fadeIn("slow");
+						var photoContent = "<a href= " + photoLink + "  target='_blank'><img src= " + photoImage + " /><span class = overlay><span class = overlayInner><h3>" + photoCaption + "<h3/><span/><span/><a/>"
+						$(".images").append(photoContent).hide().fadeIn(600);
 						
 					}
 				}
@@ -30,18 +33,19 @@ function getInstagram () {
 
 $(document).ready(function(e) {	
 	
-	$('#input').focus();
+	$("#input").focus();
 	
-	$('#input').keypress(function(e) {
+	$("#input").keypress(function(e) {
 	   if (e.keyCode==13){
 			submit();
 		}
     });
 	
 	function submit() {
-		var tag = document.getElementById("input").value;
-		getInstagram();
-		console.log(tag);
+		$(".images").empty();
+		tag = document.getElementById("input").value;
+		getInstagram(tag);
+		
 	};
 	
 	
