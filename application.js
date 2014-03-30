5ba1bccf4fa5659281cfa9d80a28284ad0f82c31
yeefy <yftran@gmail.com>
2014-03-29 21:59:22 -0700

    upload 5

--- application.js
+++ application.js
@@ -4,6 +4,7 @@
 //var code = "dd8ef8eb6d104f99b1716fb145de508c";
 
 var tag = "";
+var nextTag = "";
 
 
 function getInstagram () {
@@ -23,13 +24,38 @@ function getInstagram () {
 						var photoLink = photo.data[i].link;
 						var photoContent = "<a href= " + photoLink + "  target='_blank'><img src= " + photoImage + " /><span class = overlay><span class = overlayInner><h3>" + photoCaption + "<h3/><span/><span/><a/>"
 						$(".images").append(photoContent).hide().fadeIn(600);
-						
+						$(".viewmore").attr("hidden", false).hide().fadeIn(700);
+						nextTag = photo.pagination.next_max_tag_id;
 					}
-				}
+				}				
 			}
 		})
 	}
 
+function pagination () {
+	var accessToken = "285306793.a2755c3.6da79bffc3fa4a8b88eb3ee051058f4e";
+	var instagramAPI = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + accessToken + "&callback=?&count=18";
+	var morePhotos = instagramAPI + " &max_tag_id=" + nextTag;
+	
+	
+	$.ajax({
+			dataType:"jsonp",
+			url: morePhotos,
+			success: function(photo){
+				if (photo.data.length>0){
+					for (i=0; i<=photo.data.length; i++){
+						var photoImage = photo.data[i].images.low_resolution.url;
+						var photoCaption = photo.data[i].caption.text;
+						var photoLink = photo.data[i].link;
+						var photoContent = "<a href= " + photoLink + "  target='_blank'><img src= " + photoImage + " /><span class = overlay><span class = overlayInner><h3>" + photoCaption + "<h3/><span/><span/><a/>"
+						$(".images").append(photoContent).hide().fadeIn(600);
+						$(".viewmore").attr("hidden", false).hide().fadeIn(700);
+						nextTag = photo.pagination.next_max_tag_id;
+					}
+				}				
+			}
+		})
+}
 
 $(document).ready(function(e) {	
 	
@@ -45,11 +71,11 @@ $(document).ready(function(e) {
 		$(".images").empty();
 		tag = document.getElementById("input").value;
 		getInstagram(tag);
-		
 	};
 	
-	
-	
+	$(".viewmore").click(function(e) {
+        pagination();
+    });
 	
 });
 
